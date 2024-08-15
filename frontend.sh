96fabdd2d2786f2a7c529e479cf761e1849ac72d
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-14 21:07:33 -0700

    ALL

--- frontend.sh
+++ frontend.sh
@@ -47,3 +47,8 @@ VALIDATE $? "start nginx"
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>LOG_FILE
 VALIDATE $? "start nginx"
 
+cp -rf /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+
+systemctl restart nginx &>>LOG_FILE
+VALIDATE $? "start nginx"
+
