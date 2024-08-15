3ca2c5515edb89160862160ed8637bbd1adf8042
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-14 19:32:49 -0700

    ALL

--- mysql.sh
+++ mysql.sh
@@ -38,3 +38,21 @@ fi
 dnf install mysql-server -y &>>LOG_FILE
 VALIDATE $? "Instllation of mysql-server"
 
+systemctl enable mysqld &>>LOG_FILE
+VALIDATE $? "enable mysqld"
+
+systemctl start mysqld &>>LOG_FILE
+VALIDATE $? "start mysqld"
+
+
+mysql -h db.narendra.shop -uroot -p${mysql_root_password} -e "Sshow databases" &>>LOG_FILE
+
+if [ $? -eq 0 ]
+then 
+    echo "Password already set $Y SKIPPING $N"
+else 
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>LOG_FILE
+    VALIDATE $? "start mysqld"
+
+fi
+
