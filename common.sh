197dbdd4acbc36dae745b3a7b1ed638835b847b4
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-16 19:11:34 -0700

    ALL

---
+++ common.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+USER_ID=$(id -u)
+TIMESTAMP=$(date +%F-%M-%H-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 $R $FAILURE $N"
+    else 
+        echo -e "$2 $G SUCCESS $N"
+
+
+    fi
+}
+
+
+
+
+# if [ $USER_ID -ne 0 ]
+# then
+#     echo "Please be a super user"
+#     exit 1
+# else 
+#     echo "You are super user"
+# fi
+
+check_root(){
+    if [ $USER_ID -ne 0 ]
+    then
+        echo "Please be a super user"
+        exit 1
+    else 
+        echo "You are super user"
+    fi
+}
--- mysql.sh
+++ mysql.sh
@@ -1,39 +1,11 @@
 #!/bin/bash
-USER_ID=$(id -u)
-TIMESTAMP=$(date +%F-%M-%H-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-Y="\e[33m"
+
+source ./common.sh 
+
+check_root()
+
 echo "Enter DB password:ExpenseApp@1"
 read -s mysql_root_password
-
-VALIDATE()
-{
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 $R $FAILURE $N"
-    else 
-        echo -e "$2 $G SUCCESS $N"
-
-
-    fi
-}
-
-
-
-
-if [ $USER_ID -ne 0 ]
-then
-    echo "Please be a super user"
-    exit 1
-else 
-    echo "You are super user"
-fi
-
-
 
 dnf install mysql-server -y &>>LOG_FILE
 VALIDATE $? "Instllation of mysql-server"
