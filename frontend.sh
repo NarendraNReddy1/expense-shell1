8e5cc5c4130fcba40c0772c586fb17958c32115d
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-16 19:43:36 -0700

    ALL

--- frontend.sh
+++ frontend.sh
@@ -18,6 +18,12 @@ VALIDATE $? "start nginx"
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>LOG_FILE
 VALIDATE $? "start nginx"
 
+cd /usr/share/nginx/html &>>LOG_FILE
+VALIDATE $? "Moved to HTML"
+
+unzip /tmp/frontend.zip &>>LOG_FILE
+VALIDATE $? "Unzip the code"
+
 cp -rf /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>LOG_FILE
 VALIDATE $? "Copy code"
 
